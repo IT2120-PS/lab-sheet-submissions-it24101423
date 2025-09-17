23b8e12a8605e078b7fa10643886dd6c202fd88c
it24101423 <it24101423@my.sliit.lk>
2025-09-17 10:21:50 +0530

    Add files via upload

---
+++ Lab Sheet 06/Lab6_Answers.R
@@ -0,0 +1,23 @@
+setwd('C:\\Users\\ASUS\\OneDrive\\Desktop\\IT24101423_Lab6_PS')
+getwd()
+
+##Part-1
+#i)
+#Binomial Distribution
+
+#ii)
+#P(X>=47) = 1-P(X<=46)
+1-pbinom(46,50,0.85,TRUE)
+
+##Part-2
+#i)
+#X= Number of calls received in one hour
+
+#ii)
+#Poisson Distribution
+
+
+#iii)
+#X=15
+dpois(15,12)
+
